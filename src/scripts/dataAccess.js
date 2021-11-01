28bf17e71c9918bf3b1596a7a07ae3f497b539a8
Haley Wilson <haleyjwilson20@gmail.com>
2021-11-01 13:51:11 -0500

    Updated the database import and export functions

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -2,3 +2,20 @@ const applicationState = {
 
 }
 
+const API = "http://localhost:8088"
+
+export const fetchRequests = () => {
+    return fetch(`${API}/requests`)
+        .then(response => response.json())
+        .then(
+            (serviceRequests) => {
+                // Store the external state in application state
+                applicationState.requests = serviceRequests
+            }
+        )
+}
+
+export const getRequests = () => {
+    return database.requests.map(request => ({ ... request }))
+}
+
